640829effb061fd6e03f6a990215edaeedc6adda
Luis Otavio <luis040396@hotmail.com>
2020-06-14 16:28:08 -0300

    Adding snake :)

--- src/js/connector.js
+++ src/js/connector.js
@@ -21,7 +21,7 @@ window.TrelloPowerUp.initialize({
 			callback: function(t) {
 				return t.popup({
 					title: "Estimation",
-					url: 'estimate.html'
+					url: 'snake.html'
 				});
 			}
 		}];
