6dfc665eff0ddde2e95abf81f6031093e3fcd5bf
Luis Otavio <luis040396@hotmail.com>
2020-06-14 17:06:37 -0300

    Testing to access the checklist 4

--- src/js/connector.js
+++ src/js/connector.js
@@ -14,20 +14,20 @@ window.TrelloPowerUp.initialize({
 			}];
 		});
 	},
-	'card-badges': function(t, opts) {
-		// Return an array of card badges
-		// return [];
-		return t.card('labels')
-		.then(function(card) {
-			console.log(card);
-			if (card.labels) {
-				console.log(card.labels[0].toString());
-				return [{
-					text: card.labels[0].toString()
-				}];
-			}
-		});
-	},
+	// 'card-badges': function(t, opts) {
+	// 	// Return an array of card badges
+	// 	// return [];
+	// 	return t.card('labels')
+	// 	.then(function(card) {
+	// 		console.log(card);
+	// 		if (card.labels) {
+	// 			console.log(card.labels[0].toString());
+	// 			return [{
+	// 				text: card.labels[0].toString()
+	// 			}];
+	// 		}
+	// 	});
+	// },
 	'card-buttons': function(t, opts) {
 		return [{
 			icon: 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421',
